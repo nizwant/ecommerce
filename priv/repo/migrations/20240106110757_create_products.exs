5ec3db7ac42436b16b188086d2af066f8750f4e6
nizwant <mateusznizwantowski@gmail.com>
2024-01-06 12:14:22 +0100

    changes to schema

--- priv/repo/migrations/20240106110757_create_products.exs
+++ priv/repo/migrations/20240106110757_create_products.exs
@@ -3,10 +3,10 @@ defmodule Ecommerce.Repo.Migrations.CreateProducts do
 
   def change do
     create table(:products) do
-      add :title, :string
-      add :description, :string
-      add :price, :decimal
-      add :views, :integer
+      add(:title, :string)
+      add(:description, :string)
+      add(:price, :decimal, precision: 15, scale: 6, null: false)
+      add(:views, :integer, default: 0, null: false)
 
       timestamps(type: :utc_datetime)
     end
