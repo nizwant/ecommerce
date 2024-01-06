dd5e3af0d93f8f98646e26cf589f13c4584bd5bb
nizwant <mateusznizwantowski@gmail.com>
2024-01-06 13:05:19 +0100

    script to populate categories and linking for real now categories and products

--- lib/ecommerce/catalog/product.ex
+++ lib/ecommerce/catalog/product.ex
@@ -1,12 +1,15 @@
 defmodule Ecommerce.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecommerce.Catalog.Category
 
   schema "products" do
-    field :description, :string
-    field :title, :string
-    field :price, :decimal
-    field :views, :integer
+    field(:description, :string)
+    field(:title, :string)
+    field(:price, :decimal)
+    field(:views, :integer)
+
+    many_to_many(:categories, Category, join_through: "product_categories", on_replace: :delete)
 
     timestamps(type: :utc_datetime)
   end
