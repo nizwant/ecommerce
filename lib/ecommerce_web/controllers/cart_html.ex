d06ccc21a26eb3ef8b2c72622156ba78311644ba
nizwant <mateusznizwantowski@gmail.com>
2024-01-08 18:30:19 +0100

    Add cart controller and HTML templates

---
+++ lib/ecommerce_web/controllers/cart_html.ex
@@ -0,0 +1,9 @@
+defmodule EcommerceWeb.CartHTML do
+  use EcommerceWeb, :html
+
+  alias Ecommerce.ShoppingCart
+
+  embed_templates "cart_html/*"
+
+  def currency_to_str(%Decimal{} = val), do: "$#{Decimal.round(val, 2)}"
+end
